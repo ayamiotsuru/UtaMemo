a5d1824c0a3f3e70f58ceb61675f6683f7f2ee28
OtsuruAyami <>
2025-08-28 12:08:47 +0900

    エラーメッセージの表示

--- utamemo-app/app/Http/Controllers/PostCotroller.php
+++ utamemo-app/app/Http/Controllers/PostCotroller.php
@@ -10,7 +10,7 @@ class PostCotroller extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()//一覧表示
+    public function index() //一覧表示
     {
         //
     }
@@ -18,7 +18,7 @@ class PostCotroller extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()//新規作成フォーム
+    public function create() //新規作成フォーム
     {
         return view('post.create');
     }
@@ -26,21 +26,31 @@ class PostCotroller extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)//新規保存処理
+    public function store(Request $request) //新規保存処理
     {
-        $post = Post::create([
-            'status' => $request->status,
-            'song' => $request->song,
-            'artist' => $request->artist,
-            'comment' => $request->comment,
+        $validated = $request->validate([
+            'status' => 'required',
+            'song' => 'required|max:150',
+            'artist' => 'required|max:150',
+            'comment' => 'required|max:400',
+        ], [
+            'status.required' => '練習中かオハコか選んでください。',
+            'song.required' => '曲名を入力してください。',
+            'song.max' => '曲名は150文字以内で入力してください。',
+            'artist.required' => '歌手名を入力してください。',
+            'artist.max' => '歌手名は150文字以内で入力してください。',
+            'comment.required' => '感想を入力してください。',
+            'comment.max' => '感想は400文字以内で入力してください。',
         ]);
+        $post = Post::create($validated);
+
         return view('post.index');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Post $post)//個別表示
+    public function show(Post $post) //個別表示
     {
         //
     }
@@ -48,7 +58,7 @@ class PostCotroller extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Post $post)//編集フォーム
+    public function edit(Post $post) //編集フォーム
     {
         //
     }
@@ -56,7 +66,7 @@ class PostCotroller extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Post $post)//更新処理
+    public function update(Request $request, Post $post) //更新処理
     {
         //
     }
@@ -64,7 +74,7 @@ class PostCotroller extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Post $post)//削除処理
+    public function destroy(Post $post) //削除処理
     {
         //
     }
--- utamemo-app/resources/views/post/create.blade.php
+++ utamemo-app/resources/views/post/create.blade.php
@@ -10,22 +10,29 @@
             <div class="mt-8">
                 <div class="w-full flex flex-col">
                     <label for="song" class="font-semibold mt-4">曲名：</label>
-                    <input type="text" name="song" class="w-auto pu-2 border border-gray-300 rounded-md" id="song">
+                    <x-input-error :messages="$errors->get('song')" class="mt-2" />
+                    <input type="text" name="song" class="w-auto pu-2 border border-gray-300 rounded-md"
+                        id="song" value="{{ old('song') }}">
                 </div>
             </div>
             <div class="mt-8">
                 <div class="w-full flex flex-col">
                     <label for="artist" class="font-semibold mt-4">歌手名：</label>
-                    <input type="text" name="artist" class="w-auto pu-2 border border-gray-300 rounded-md" id="artist">
+                    <x-input-error :messages="$errors->get('artist')" class="mt-2" />
+                    <input type="text" name="artist" class="w-auto pu-2 border border-gray-300 rounded-md"
+                        id="artist" value="{{ old('artist') }}">
                 </div>
             </div>
             <div class="mt-4">
+                <x-input-error :messages="$errors->get('status')" class="mt-2" />
                 <input type="radio" name="status" value="0" id="status01">練習中
                 <input type="radio" name="status" value="1" id="status02">オハコ
             </div>
             <div class="w-full flex flex-col">
                 <label for="comment" class="font-semibold mt-4">感想：</label>
-                <textarea name="comment" id="comment" cols="30" rows="10" class="w-auto py-2 border border-gray-300 rounded-md"></textarea>
+                <x-input-error :messages="$errors->get('comment')" class="mt-2" />
+                <textarea name="comment" id="comment" cols="30" rows="10"
+                    class="w-auto py-2 border border-gray-300 rounded-md" value="{{ old('artist') }}"></textarea>
             </div>
             <x-primary-button class="mt-4">
                 投稿する
