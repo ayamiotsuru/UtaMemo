6293d4da5bbd17b6b4ac7a5324c6ff53b46184e5
OtsuruAyami <>
2025-08-31 13:32:06 +0900

    commentsテーブルとカラムのマイグレーション

--- utamemo-app/database/migrations/2025_08_31_125917_create_comments_table.php
+++ utamemo-app/database/migrations/2025_08_31_125917_create_comments_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');//カラムの作成と外部キー制約、親レコード削除に連動して削除される
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');//カラムの作成と外部キー制約、親レコード削除に連動して削除される
+            $table->text('content');
             $table->timestamps();
         });
     }
